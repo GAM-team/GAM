10d6a0d454c269a03c83a7b2e7211e7a7d36b277
Jay Lee <jay0lee@gmail.com>
2014-07-04 14:35:18 -0400

    7-zip is 32-bit and stop building python source package

--- build.bat
+++ build.bat
@@ -13,36 +13,11 @@ xcopy whatsnew.txt gam\
 xcopy cacert.pem gam\
 xcopy admin-settings-v1.json gam\
 del gam\w9xpopen.exe
-"%ProgramFiles%\7-Zip\7z.exe" a -tzip gam-%1-windows.zip gam\ -xr!.svn
+"%ProgramFiles(x86)%\7-Zip\7z.exe" a -tzip gam-%1-windows.zip gam\ -xr!.svn
 
 \python27\python.exe setup-64.py py2exe
 xcopy LICENSE gam-64\
 xcopy whatsnew.txt gam-64\
 xcopy cacert.pem gam-64\
 xcopy admin-settings-v1.json gam-64\
-"%ProgramFiles%\7-Zip\7z.exe" a -tzip gam-%1-windows-x64.zip gam-64\ -xr!.svn
-
-mkdir python-src-%1
-mkdir python-src-%1\gdata
-mkdir python-src-%1\atom
-mkdir python-src-%1\apiclient
-mkdir python-src-%1\httplib2
-mkdir python-src-%1\oauth2client
-mkdir python-src-%1\simplejson
-mkdir python-src-%1\uritemplate
-xcopy gam.py python-src-%1\
-xcopy LICENSE python-src-%1\
-xcopy whatsnew.txt python-src-%1\
-xcopy /e gdata\*.* python-src-%1\gdata
-xcopy /e atom\*.* python-src-%1\atom
-xcopy /e apiclient\*.* python-src-%1\apiclient
-xcopy /e httplib2\*.* python-src-%1\httplib2
-xcopy /e oauth2client\*.* python-src-%1\oauth2client
-xcopy /e simplejson\*.* python-src-%1\simplejson
-xcopy /e uritemplate\*.* python-src-%1\uritemplate
-xcopy cacert.pem python-src-%1\
-xcopy admin-settings-v1.json python-src-%1\
-
-cd python-src-%1
-"%ProgramFiles%\7-Zip\7z.exe" a -tzip ..\gam-%1-python-src.zip * -xr!.svn
-cd ..
+"%ProgramFiles(x86)%\7-Zip\7z.exe" a -tzip gam-%1-windows-x64.zip gam-64\ -xr!.svn
