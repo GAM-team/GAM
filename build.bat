8d967b112576b778b25d28f2d588d20f0dc257c6
Jay Lee <jay0lee@gmail.com>
2015-07-02 06:38:13 -0400

    minor fixes for GAM 3.5 Windows build

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ rmdir /q /s dist
 del /q /f gam-%1-windows.zip
 del /q /f gam-%1-windows-x64.zip
 
-c:\python27-32\scripts\pyinstaller -F --distpath=gam gam.py
+c:\python27-32\scripts\pyinstaller -F --distpath=gam gam.spec
 xcopy LICENSE gam\
 xcopy whatsnew.txt gam\
 xcopy cacert.pem gam\
@@ -14,7 +14,7 @@ xcopy cloudprint-v2.json gam\
 del gam\w9xpopen.exe
 "%ProgramFiles(x86)%\7-Zip\7z.exe" a -tzip gam-%1-windows.zip gam\ -xr!.svn
 
-c:\python27\scripts\pyinstaller -F --distpath=gam-64 gam.py
+c:\python27\scripts\pyinstaller -F --distpath=gam-64 gam.spec
 xcopy LICENSE gam-64\
 xcopy whatsnew.txt gam-64\
 xcopy cacert.pem gam-64\
