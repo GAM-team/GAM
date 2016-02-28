5ab14fef0572d5b4f86c3a84410d4ac347d33c33
Ross Scroggs <ross.scroggs@gmail.com>
2016-02-27 19:07:01 -0800

    Handle missing values in column

--- src/gam.py
+++ src/gam.py
@@ -8678,7 +8678,7 @@ def getUsersToModify(entity_type=None, entity=None, silent=False, return_uids=Fa
     if (not filename) or (not column):
       systemErrorExit(2, u'Expected {0} FileName:FieldName'.format(entity_type))
     f = openFile(filename)
-    input_file = csv.DictReader(f)
+    input_file = csv.DictReader(f, restval=u'')
     if column not in input_file.fieldnames:
       systemErrorExit(2, MESSAGE_HEADER_NOT_FOUND_IN_CSV_HEADERS.format(column, ','.join(input_file.fieldnames)))
     users = []
