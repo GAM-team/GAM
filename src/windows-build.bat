4254ed4d869a0a95b8167c0ff902fc8fbc098705
Jay Lee <jay0lee@gmail.com>
2016-10-11 15:56:24 -0400

    x64 7zip, clean pyinsataller, python-64 path

--- src/windows-build.bat
+++ src/windows-build.bat
@@ -5,13 +5,13 @@ rmdir /q /s dist
 del /q /f gam-%1-windows.zip
 del /q /f gam-%1-windows-x64.zip
 
-c:\python27-32\scripts\pyinstaller -F --distpath=gam windows-gam.spec
+c:\python27-32\scripts\pyinstaller --clean -F --distpath=gam windows-gam.spec
 xcopy LICENSE gam\
 xcopy whatsnew.txt gam\
 del gam\w9xpopen.exe
-"%ProgramFiles(x86)%\7-Zip\7z.exe" a -tzip gam-%1-windows.zip gam\ -xr!.svn
+"%ProgramFiles%\7-Zip\7z.exe" a -tzip gam-%1-windows.zip gam\ -xr!.svn
 
-c:\python27\scripts\pyinstaller -F --distpath=gam-64 windows-gam.spec
+c:\python27-64\scripts\pyinstaller --clean -F --distpath=gam-64 windows-gam.spec
 xcopy LICENSE gam-64\
 xcopy whatsnew.txt gam-64\
-"%ProgramFiles(x86)%\7-Zip\7z.exe" a -tzip gam-%1-windows-x64.zip gam-64\ -xr!.svn
+"%ProgramFiles%\7-Zip\7z.exe" a -tzip gam-%1-windows-x64.zip gam-64\ -xr!.svn
