e101231f9bfa1d5ae45a2072aa8c40e2d2406e03
Jay Lee <jay0lee@gmail.com>
2016-10-12 14:53:41 -0400

    Update linux-build.sh

--- src/linux-build.sh
+++ src/linux-build.sh
@@ -3,6 +3,7 @@ rm -rf build
 rm -rf  dist
 rm -rf gam-$1-linux-$(arch).tar.xz
 
+export LD_LIBRARY_PATH=/usr/local/lib
 pyinstaller --clean -F --distpath=gam linux-gam.spec
 cp LICENSE gam
 cp whatsnew.txt gam
