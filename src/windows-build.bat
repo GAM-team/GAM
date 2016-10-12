46c000f6d1bff5e4c51a982d99802660beb6b27f
Jay Lee <jay0lee@gmail.com>
2016-10-12 11:25:26 -0400

    MSI Windows build

--- src/windows-build.bat
+++ src/windows-build.bat
@@ -4,6 +4,9 @@ rmdir /q /s build
 rmdir /q /s dist
 del /q /f gam-%1-windows.zip
 del /q /f gam-%1-windows-x64.zip
+del /q /f gam-%1-windows-x64.msi
+del /q /f gam.wixobj
+del /q /f gam.wixpdb
 
 c:\python27-32\scripts\pyinstaller --clean -F --distpath=gam windows-gam.spec
 xcopy LICENSE gam\
@@ -15,3 +18,7 @@ c:\python27-64\scripts\pyinstaller --clean -F --distpath=gam-64 windows-gam.spec
 xcopy LICENSE gam-64\
 xcopy whatsnew.txt gam-64\
 "%ProgramFiles%\7-Zip\7z.exe" a -tzip gam-%1-windows-x64.zip gam-64\ -xr!.svn
+
+set GAMVERSION=%1
+"%ProgramFiles(x86)%\WiX Toolset v3.10\bin\candle.exe" -arch x64 gam.wxs
+"%ProgramFiles(x86)%\WiX Toolset v3.10\bin\light.exe" -ext "%ProgramFiles(x86)%\WiX Toolset v3.10\bin\WixUIExtension.dll" gam.wixobj -o gam-%1-windows-x64.msi
