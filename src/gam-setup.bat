082c34b4535a5e31cc82eefffadbb172bb94cba6
Jay Lee <jay0lee@gmail.com>
2016-11-04 11:39:20 -0400

    prompt for regular user to test service account.

--- src/gam-setup.bat
+++ src/gam-setup.bat
@@ -60,8 +60,10 @@
 @  echo Please answer y or n.
 @  goto saauth
    )
+@echo(
+@set /p regularuser= "Please enter the email address of a regular G Suite user: "
 @echo Great! Checking service account scopes. This will fail the first time. Follow the steps to authorize and retry. It can take a few minutes for scopes to PASS after they've been authorized in the admin console.
-@gam user %adminemail% check serviceaccount
+@gam user %regularuser% check serviceaccount
 @if not ERRORLEVEL 1 goto sadone
 @echo(
 @echo Service account authorization failed. Confirm you entered the scopes correctly in the admin console. It can take a few minutes for scopes to PASS after they are entered in the admin console so if you're sure you entered them correctly, go grab a coffee and then hit Y to try again. Say N to skip admin authorization.
