dfaeeba9d5613d9f9cdba4b2c881e1948f7ea84b
Ross Scroggs <ross.scroggs@gmail.com>
2016-11-11 20:33:13 -0800

    Update build scripts

--- src/macos-build.sh
+++ src/macos-build.sh
@@ -1,10 +1,11 @@
-rmdir /q /s gam
-rmdir /q /s build
-rmdir /q /s dist
+rm -rf gam
+rm -rf build
+rm -rf dist
 rm -rf gam-$1-macos.tar.xz
 
 /Library/Frameworks/Python.framework/Versions/2.7/bin/pyinstaller --clean -F --distpath=gam macos-gam.spec
 cp LICENSE gam
 cp whatsnew.txt gam
+cp GamCommands.txt gam
 
 tar cfJ gam-$1-macos.tar.xz gam/ 
