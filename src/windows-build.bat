93b05de15e8b0cbf1e29d2c771df5a15db9874b4
Ross Scroggs <taers232c@users.noreply.github.com>
2016-11-12 10:07:19 -0800

    Update build scripts (#326)

--- src/windows-build.bat
+++ src/windows-build.bat
@@ -12,7 +12,7 @@ c:\python27-32\scripts\pyinstaller --clean -F --distpath=gam windows-gam.spec
 xcopy LICENSE gam\
 xcopy whatsnew.txt gam\
 xcopy gam-setup.bat gam\
-xcopy gamcommands.txt gam\
+xcopy GamCommands.txt gam\
 del gam\w9xpopen.exe
 "%ProgramFiles%\7-Zip\7z.exe" a -tzip gam-%1-windows.zip gam\ -xr!.svn
 
@@ -20,7 +20,7 @@ c:\python27-64\scripts\pyinstaller --clean -F --distpath=gam-64 windows-gam.spec
 xcopy LICENSE gam-64\
 xcopy whatsnew.txt gam-64\
 xcopy gam-setup.bat gam-64\
-xcopy gamcommands.txt gam-64\
+xcopy GamCommands.txt gam-64\
 "%ProgramFiles%\7-Zip\7z.exe" a -tzip gam-%1-windows-x64.zip gam-64\ -xr!.svn
 
 set GAMVERSION=%1
