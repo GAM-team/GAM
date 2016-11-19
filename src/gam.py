3669cfc5e8717a81115c033846f0545b66afbcfb
Ross Scroggs <ross.scroggs@gmail.com>
2016-11-19 08:59:23 -0800

    Fix Windows multiprocessing

--- src/gam.py
+++ src/gam.py
@@ -39,6 +39,7 @@ from htmlentitydefs import name2codepoint
 from HTMLParser import HTMLParser
 import json
 import mimetypes
+from multiprocessing import freeze_support
 import platform
 import random
 import re
@@ -10920,6 +10921,38 @@ def ProcessGAMCommand(args):
     GM_Globals[GM_SYSEXITRC] = e.code
   return GM_Globals[GM_SYSEXITRC]
 
+#
+# From: https://github.com/pyinstaller/pyinstaller/wiki/Recipe-Multiprocessing
+#
+if sys.platform.startswith('win'):
+  try:
+    import multiprocessing.popen_spawn_win32 as forking
+  except ImportError:
+    import multiprocessing.forking as forking
+
+  # First define a modified version of Popen.
+  class _Popen(forking.Popen):
+    def __init__(self, *args, **kw):
+      if hasattr(sys, 'frozen'):
+        # We have to set original _MEIPASS2 value from sys._MEIPASS
+        # to get --onefile mode working.
+        os.putenv('_MEIPASS2', sys._MEIPASS)
+      try:
+        super(_Popen, self).__init__(*args, **kw)
+      finally:
+        if hasattr(sys, 'frozen'):
+          # On some platforms (e.g. AIX) 'os.unsetenv()' is not
+          # available. In those cases we cannot delete the variable
+          # but only set it to the empty string. The bootloader
+          # can handle this case.
+          if hasattr(os, 'unsetenv'):
+            os.unsetenv('_MEIPASS2')
+          else:
+            os.putenv('_MEIPASS2', '')
+
+  # Second override 'Popen' class with our modified version.
+  forking.Popen = _Popen
+
 def win32_unicode_argv():
   from ctypes import POINTER, byref, cdll, c_int, windll
   from ctypes.wintypes import LPCWSTR, LPWSTR
@@ -10942,6 +10975,7 @@ def win32_unicode_argv():
 
 # Run from command line
 if __name__ == "__main__":
+  freeze_support()
   reload(sys)
   if sys.version_info[:2] != (2, 7):
     print u'ERROR: GAM requires Python 2.7. You are running %s.%s.%s. Please upgrade your Python version or use one of the binary GAM downloads.' % sys.version_info[:3]
