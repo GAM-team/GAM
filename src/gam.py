4b53e883405ffddeda789aa70ca64ccac996d76c
Ross Scroggs <ross.scroggs@gmail.com>
2017-07-29 06:02:15 -0700

    In update matter, only fetch old body if necessary

--- src/gam.py
+++ src/gam.py
@@ -7416,9 +7416,10 @@ def doUpdateVaultMatter(action=None):
     action_kwargs = {}
   if body:
     print u'Updating matter %s...' % sys.argv[3]
-    # bah, API requires name to be sent on update even when it's not changing
-    result = callGAPI(v.matters(), u'get', matterId=matterId, view=u'BASIC')
-    body.setdefault(u'name', result[u'name'])
+    if u'name' not in body:
+      # bah, API requires name to be sent on update even when it's not changing
+      result = callGAPI(v.matters(), u'get', matterId=matterId, view=u'BASIC')
+      body[u'name'] = result[u'name']
     callGAPI(v.matters(), u'update', body=body, matterId=matterId)
   if action:
     print u'Performing %s on matter %s' % (action, sys.argv[3])
