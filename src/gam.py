6f4613d444c731552816df0dcdd5c6f344412f5c
Ross Scroggs <ross.scroggs@gmail.com>
2019-04-12 08:28:21 -0700

    Update gam.py

--- src/gam.py
+++ src/gam.py
@@ -13126,7 +13126,7 @@ Append an 'r' to grant read-only access or an 'a' to grant action-only access.
       if selected is None:
         # Toggle the option on/off
         option.is_selected = not option.is_selected
-        option._restriction = None
+        option.unrestrict()
       else:
         option.is_selected = selected
     else:
