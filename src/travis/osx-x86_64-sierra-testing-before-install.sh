48553f4ad98f4461091d4d66aa62dad00138b4ba
Jay Lee <jay0lee@gmail.com>
2019-04-15 13:09:05 -0400

    Linux / MacOS changes

--- src/travis/osx-x86_64-sierra-testing-before-install.sh
+++ src/travis/osx-x86_64-sierra-testing-before-install.sh
@@ -1,6 +1,6 @@
-sudo brew update
-sudo brew install python@2 || true
-sudo brew link --overwrite python@2
+brew update
+brew install python@2 || true
+brew link --overwrite python@2
 sudo pip install --upgrade pip
 sudo pip freeze > requirements.txt
 sudo pip install --upgrade -r requirements.txt
