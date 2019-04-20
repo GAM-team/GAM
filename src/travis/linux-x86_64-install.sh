1223a707c1ecea9406dc1353a4a9558e1f0176ba
Jay Lee <jay0lee@gmail.com>
2019-04-20 18:09:51 -0400

    log size and run times

--- src/travis/linux-x86_64-install.sh
+++ src/travis/linux-x86_64-install.sh
@@ -8,9 +8,15 @@ cp GamCommands.txt gam
 echo "PyInstaller GAM is:"
 du -ch gam/gam
 staticx gam/gam gam/gam-static
-rm gam/gam
+mv gam/gam gam/gam-linked
 mv gam/gam-static gam/gam
 echo "StaticX GAM is:"
 du -ch gam/gam
+echo "PyInstaller GAM time is:"
+gam/gam-linked version check
+echo "StaticX GAM is:"
+gam/gam version check
 GAM_ARCHIVE=gam-$GAMVERSION-$GAMOS-$PLATFORM.tar.xz
 tar cfJ $GAM_ARCHIVE gam/
+echo "Archive size is:"
+du -ch $GAM_ARCHIVE
