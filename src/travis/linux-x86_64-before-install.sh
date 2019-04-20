f206774fa9a0bef151c63c5c07950deff4ce7d15
Jay Lee <jay0lee@gmail.com>
2019-04-20 17:55:52 -0400

    StaticX for realz

--- src/travis/linux-x86_64-before-install.sh
+++ src/travis/linux-x86_64-before-install.sh
@@ -1,5 +1,7 @@
-sudo apt-get --yes update
-sudo apt --yes dist-upgrade
+echo "RUNNING: apt update..."
+sudo apt --yes update > /dev/null
+echo "RUNNING: apt dist-upgrade..."
+sudo apt --yes dist-upgrade > /dev/null
 pip freeze > requirements.txt
 pip install --upgrade -r requirements.txt
 sudo rm requirements.txt
--- src/travis/linux-x86_64-install.sh
+++ src/travis/linux-x86_64-install.sh
@@ -5,5 +5,12 @@ export GAMVERSION=`gam/gam version simple`
 cp LICENSE gam
 cp whatsnew.txt gam
 cp GamCommands.txt gam
+echo "PyInstaller GAM is:"
+du -ch gam/gam
+staticx gam/gam gam/gam-static
+rm gam/gam
+mv gam/gam-static gam/gam
+echo "StaticX GAM is:"
+du -ch gam/gam
 GAM_ARCHIVE=gam-$GAMVERSION-$GAMOS-$PLATFORM.tar.xz
 tar cfJ $GAM_ARCHIVE gam/
