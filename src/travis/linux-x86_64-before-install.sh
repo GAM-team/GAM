5e63ce3b165ec903ef0f8eba205e27cc56a404fc
Jay Lee <jay0lee@gmail.com>
2019-04-20 08:21:04 -0400

    Update linux-x86_64-before-install.sh

--- src/travis/linux-x86_64-before-install.sh
+++ src/travis/linux-x86_64-before-install.sh
@@ -1,8 +1,8 @@
 sudo apt-get --yes update
 sudo apt --yes dist-upgrade
 sudo apt --yes install build-essential checkinstall libreadline-gplv2-dev libncursesw5-dev libssl-dev libc6-dev libbz2-dev
-wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.xz
-tar xvf Python-3.7.3.xz
+wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tar.xz
+tar xvf Python-3.7.3.tar.xz
 cd Python-3.7.3
 ./configure --enable-optimizations
 sudo make altinstall
