7517886d56c62bc47546ff600fba3b78d40caf80
Jay Lee <jay0lee@gmail.com>
2019-04-20 09:29:29 -0400

    quiet down the make

--- src/travis/linux-x86_64-before-install.sh
+++ src/travis/linux-x86_64-before-install.sh
@@ -2,10 +2,10 @@ sudo apt-get --yes update
 sudo apt --yes dist-upgrade
 sudo apt --yes install build-essential checkinstall libreadline-gplv2-dev libncursesw5-dev libssl-dev libc6-dev libbz2-dev
 wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tar.xz
-tar xvf Python-3.7.3.tar.xz
+tar xf Python-3.7.3.tar.xz
 cd Python-3.7.3
 ./configure --enable-optimizations
-sudo make altinstall
+sudo make altinstall > /dev/null || sudo make altinstall
 PATH=/usr/local/bin:$PATH
 ls -alRF /usr/local/bin
 echo "PYTHON -V"
