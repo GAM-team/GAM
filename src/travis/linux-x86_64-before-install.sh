0c1341998aa0b7c0b92569e51b442586b5989a12
Jay Lee <jay0lee@gmail.com>
2019-04-20 12:28:59 -0400

    pip3

--- src/travis/linux-x86_64-before-install.sh
+++ src/travis/linux-x86_64-before-install.sh
@@ -7,13 +7,12 @@ wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tar.xz
 tar xf Python-3.7.3.tar.xz
 cd Python-3.7.3
 sudo ./configure --enable-optimizations
-echo "BEFORE"
-ls -al /usr/bin
-sudo make install
-echo "AFTER"
-ls -al /usr/bin
-sudo /usr/bin/pip3 install --upgrade pip
-sudo /usr/bin/pip3 freeze > requirements.txt
-sudo /usr/bin/pip3 install --upgrade -r requirements.txt
+sudo make altinstall
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo /usr/local/bin/python3.7 get-pip.py
+ls -alRF /usr/local/bin
+sudo /usr/local/bin/pip install --upgrade pip
+sudo /usr/local/bin/pip freeze > requirements.txt
+sudo /usr/local/bin/pip install --upgrade -r requirements.txt
 sudo rm requirements.txt
-sudo /usr/bin/pip3 pip install pyinstaller
+sudo /usr/local/bin/pip pip install pyinstaller
