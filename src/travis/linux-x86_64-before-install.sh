c673cb626b4908d3496db1f33a60c155dcfced8c
Jay Lee <jay0lee@gmail.com>
2019-04-20 09:46:12 -0400

    alias

--- src/travis/linux-x86_64-before-install.sh
+++ src/travis/linux-x86_64-before-install.sh
@@ -6,19 +6,20 @@ tar xf Python-3.7.3.tar.xz
 cd Python-3.7.3
 ./configure --enable-optimizations
 sudo make altinstall > /dev/null || sudo make altinstall
+alias python=/usr/local/bin/python3.7
+alias python3=/usr/local/bin/python3.7
+alias python37=/usr/local/bin/python3.7
+alias pip=/usr/local/bin/pip3.7
+alias pip3=/usr/local/bin/pip3.7
+alias pip37=/usr/local/bin/pip3.7
 PATH=/usr/local/bin:$PATH
 ls -alRF /usr/local/bin
 echo "PYTHON -V"
-/usr/local/bin/python -V
+python -V
 echo "PYTHON3 -V"
-/usr/local/bin/python3 -V
-echo "general PYTHON -V"
-/usr/local/bin/python -V
 python3 -V
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-sudo python get-pip.py
-pip3 install --upgrade pip
-pip3 freeze > requirements.txt
-pip3 install --upgrade -r requirements.txt
+pip install --upgrade pip
+pip freeze > requirements.txt
+pip install --upgrade -r requirements.txt
 rm requirements.txt
-pip3 install pyinstaller
+pip install pyinstaller
