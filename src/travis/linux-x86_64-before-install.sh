f4a10775ca1b50aff9df36358885581cea1c4279
Jay Lee <jay0lee@gmail.com>
2019-04-20 10:28:03 -0400

    again

--- src/travis/linux-x86_64-before-install.sh
+++ src/travis/linux-x86_64-before-install.sh
@@ -4,22 +4,10 @@ sudo apt --yes install build-essential checkinstall libreadline-gplv2-dev libncu
 wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tar.xz
 tar xf Python-3.7.3.tar.xz
 cd Python-3.7.3
-./configure --enable-optimizations
-sudo make altinstall > /dev/null || sudo make altinstall
-alias python=/usr/local/bin/python3.7
-alias python3=/usr/local/bin/python3.7
-alias python37=/usr/local/bin/python3.7
-alias pip=/usr/local/bin/pip3.7
-alias pip3=/usr/local/bin/pip3.7
-alias pip37=/usr/local/bin/pip3.7
-PATH=/usr/local/bin:$PATH
-ls -alRF /usr/local/bin
-echo "PYTHON -V"
-python -V
-echo "PYTHON3 -V"
-python3 -V
-pip install --upgrade pip
-pip freeze > requirements.txt
-pip install --upgrade -r requirements.txt
-rm requirements.txt
-pip install pyinstaller
+sudo ./configure --enable-optimizations
+sudo make install > /dev/null || sudo make altinstall
+sudo /usr/bin/pip3.7 install --upgrade pip
+sudo /usr/bin/pip3.7 freeze > requirements.txt
+sudo /usr/bin/pip3.7 install --upgrade -r requirements.txt
+sudo rm requirements.txt
+sudo /usr/bin/pip3.7 pip install pyinstaller
