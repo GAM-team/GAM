dc6ff493feb940f9028a9ec8dfd28a74809ea65b
Jay Lee <jay0lee@gmail.com>
2019-04-20 18:02:41 -0400

    Install StaticX deps

--- src/travis/linux-x86_64-before-install.sh
+++ src/travis/linux-x86_64-before-install.sh
@@ -1,7 +1,10 @@
 echo "RUNNING: apt update..."
-sudo apt --yes update > /dev/null
+sudo apt-get --yes update > /dev/null
 echo "RUNNING: apt dist-upgrade..."
-sudo apt --yes dist-upgrade > /dev/null
+sudo apt-get --yes dist-upgrade > /dev/null
+echo "Installing StaticX deps..."
+sudo apt-get --yes install binutils patchelf
+echo "Upgrading pip packages..."
 pip freeze > requirements.txt
 pip install --upgrade -r requirements.txt
 sudo rm requirements.txt
