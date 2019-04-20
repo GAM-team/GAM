4d29db9bf746c33ae7558f226ee981695cdbd335
Jay Lee <jay0lee@gmail.com>
2019-04-20 06:58:05 -0400

    use daadsnakes PPA

--- src/travis/linux-x86_64-before-install.sh
+++ src/travis/linux-x86_64-before-install.sh
@@ -1,3 +1,7 @@
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt dist-upgrade
+sudo apt install python3
 pip install --upgrade pip
 pip freeze > requirements.txt
 pip install --upgrade -r requirements.txt
