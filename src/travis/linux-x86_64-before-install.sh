f4167424fa2b0d04217f5454ef291dd88119cbf3
Jay Lee <jay0lee@gmail.com>
2019-04-20 07:06:57 -0400

    --yes

--- src/travis/linux-x86_64-before-install.sh
+++ src/travis/linux-x86_64-before-install.sh
@@ -1,7 +1,7 @@
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt-get update
-sudo apt dist-upgrade
-sudo apt install python3
+sudo add-apt-repository --yes ppa:deadsnakes/ppa
+sudo apt-get --yes update
+sudo apt --yes dist-upgrade
+sudo apt --yes install python3
 pip install --upgrade pip
 pip freeze > requirements.txt
 pip install --upgrade -r requirements.txt
