f6672496d15c67ee8cd5aec4a549e1003c0798ac
Jay Lee <jay0lee@gmail.com>
2019-05-05 12:55:00 -0400

    Update windows-x86-before-install.sh

--- src/travis/windows-x86-before-install.sh
+++ src/travis/windows-x86-before-install.sh
@@ -1,9 +1,9 @@
-powershell Install-WindowsFeature Net-Framework-Core
+until powershell Install-WindowsFeature Net-Framework-Core; do echo "trying again..."; done
 cinst -y --forcex86 python3
 cinst -y --forcex86 openssl.light
 cp -v /c/Program\ Files/OpenSSL/*.dll /c/Python37/DLLs
 export PATH=$PATH:/c/Python37/scripts
-cinst -y wixtoolset
+until cinst -y wixtoolset; do echo "trying again..."; done
 pip install --upgrade pip
 pip freeze > upgrades.txt
 pip install --upgrade -r upgrades.txt
