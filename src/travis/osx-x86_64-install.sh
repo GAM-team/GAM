f972f0b8e45c29b6b68011369cd755362bc23c63
Jay Lee <jay0lee@gmail.com>
2019-05-07 17:12:26 -0400

    really fix osx gampath

--- src/travis/osx-x86_64-install.sh
+++ src/travis/osx-x86_64-install.sh
@@ -1,7 +1,7 @@
 cd src
 $python -OO -m PyInstaller --clean --debug -F --distpath=gam $GAMOS-gam.spec
 export gam="gam/gam"
-export gampath=~/gam
+export gampath=gam
 $gam version extended
 export GAMVERSION=`gam/gam version simple`
 cp LICENSE gam
