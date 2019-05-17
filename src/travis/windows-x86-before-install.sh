33c85e9ed6ddfb1d5adce83e602e89102e930bbc
Jay Lee <jay0lee@gmail.com>
2019-05-17 15:18:41 -0400

    allow PRs to succeed, Win SSL 1.1.1b

--- src/travis/windows-x86-before-install.sh
+++ src/travis/windows-x86-before-install.sh
@@ -1,6 +1,11 @@
 until powershell Install-WindowsFeature Net-Framework-Core; do echo "trying again..."; done
 cinst -y --forcex86 python3
-cinst -y --forcex86 openssl.light
+#cinst -y --forcex86 openssl.light
+cd ~/pybuild
+if [ ! -e Win32OpenSSL_Light-1_1_1b.exe ]; then
+  wget --quiet https://slproweb.com/download/Win32OpenSSL_Light-1_1_1b.exe
+fi
+./Win32OpenSSL_Light-1_1_1b.exe /silent /sp- /suppressmsgboxes /DIR=$ProgramFiles\OpenSSL -f
 cp -v /c/Program\ Files/OpenSSL/*.dll /c/Python37/DLLs
 export PATH=$PATH:/c/Python37/scripts
 until cinst -y wixtoolset; do echo "trying again..."; done
