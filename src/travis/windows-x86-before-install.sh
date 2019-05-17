5f0644d9246207f0bf878ba4ecb83e4494c82143
Jay Lee <jay0lee@gmail.com>
2019-05-17 16:01:08 -0400

    extract msi for openssl

--- src/travis/windows-x86-before-install.sh
+++ src/travis/windows-x86-before-install.sh
@@ -1,12 +1,19 @@
 until powershell Install-WindowsFeature Net-Framework-Core; do echo "trying again..."; done
 cinst -y --forcex86 python3
-#cinst -y --forcex86 openssl.light
 cd ~/pybuild
-if [ ! -e Win32OpenSSL_Light-1_1_1b.exe ]; then
-  wget --quiet https://slproweb.com/download/Win32OpenSSL_Light-1_1_1b.exe
+msifile=Win32OpenSSL_Light-${BUILD_OPENSSL_VERSION//./_}.msi
+if [ ! -e $msifile ]; then
+  echo "Downloading $msifile..."
+  wget --quiet https://slproweb.com/download/$msifile
 fi
-./Win32OpenSSL_Light-1_1_1b.exe /silent /sp- /suppressmsgboxes /DIR=$ProgramFiles\OpenSSL -f
-cp -v /c/Program\ Files/OpenSSL/*.dll /c/Python37/DLLs
+if { ! -e ssl/libeay32.dll ]; then
+  msiexec /a $msifile /qn TARGETDIR=ssl
+fi
+echo "OpenSSL dlls..."
+ls -al ssl
+echo "Python dlls..."
+ls -al /c/Python37/DLLs
+cp -v ssl/*.dll /c/Python37/DLLs
 export PATH=$PATH:/c/Python37/scripts
 until cinst -y wixtoolset; do echo "trying again..."; done
 pip install --upgrade pip
--- src/travis/windows-x86_64-before-install.sh
+++ src/travis/windows-x86_64-before-install.sh
@@ -1,10 +1,17 @@
 until powershell Install-WindowsFeature Net-Framework-Core; do echo "trying again..."; done
 cinst -y python3
 cd ~/pybuild
-if [ ! -e Win64OpenSSL_Light-1_1_1b.exe ]; then
-  wget --quiet https://slproweb.com/download/Win64OpenSSL_Light-1_1_1b.exe
+msifile=Win64OpenSSL_Light-${BUILD_OPENSSL_VERSION//./_}.msi
+if [ ! -e $msifile ]; then
+  wget --quiet https://slproweb.com/download/$msifile
 fi
-./Win64OpenSSL_Light-1_1_1b.exe /silent /sp- /suppressmsgboxes /DIR=$ProgramFiles\OpenSSL -f
+if { ! -e ssl/libeay32.dll ]; then
+  msiexec /a $msifile /qn TARGETDIR=ssl
+fi
+echo "OpenSSL dlls..."
+ls -al ssl
+echo "Python dlls..."
+ls -al /c/Python37/DLLs
 cp -v /c/Program\ Files/OpenSSL/bin/*.dll /c/Python37/DLLs
 export PATH=$PATH:/c/Python37/scripts
 until cinst -y wixtoolset; do echo "trying again..."; done
