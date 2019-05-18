8b1c8b36ce5fadfe7b9c56845469b930163251a5
Jay Lee <jay0lee@gmail.com>
2019-05-18 07:21:47 -0400

    Update windows-x86-before-install.sh

--- src/travis/windows-x86-before-install.sh
+++ src/travis/windows-x86-before-install.sh
@@ -1,4 +1,5 @@
 echo "Installing Net-Framework-Core..."
+export mypath=$(pwd)
 until powershell Install-WindowsFeature Net-Framework-Core; do echo "trying again..."; done
 cinst -y --forcex86 python3
 cd ~/pybuild
@@ -9,16 +10,16 @@ if [ ! -e $exefile ]; then
 fi
 if [ ! -e ssl/libssl-1_1-x64.dll ]; then
   echo "Installing $exefile..."
-  powershell ".\\${exefile} /silent /sp- /suppressmsgboxes /DIR=C:\\Users\\travis\\pybuild\\ssl"
+  powershell ".\\${exefile} /silent /sp- /suppressmsgboxes /DIR=C:\\ssl"
 fi
 echo "OpenSSL dlls..."
-ls -alRF ssl
+ls -alRF /c/ssl
 echo "Python dlls..."
 ls -al /c/Python37/DLLs
-cp -v ssl/*.dll /c/Python37/DLLs
+cp -v /c/ssl/*.dll /c/Python37/DLLs
 export PATH=$PATH:/c/Python37/scripts
 until cinst -y wixtoolset; do echo "trying again..."; done
-cd ~
+cd $mypath
 pip install --upgrade pip
 pip freeze > upgrades.txt
 pip install --upgrade -r upgrades.txt
