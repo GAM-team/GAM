59d8e5a853f69024b6324f8a6de74a848b1f94e2
Jay Lee <jay0lee@gmail.com>
2019-05-18 10:40:03 -0400

    change order to allow more time for processes to finish

--- src/travis/windows-x86_64-before-install.sh
+++ src/travis/windows-x86_64-before-install.sh
@@ -1,7 +1,6 @@
 echo "Installing Net-Framework-Core..."
 export mypath=$(pwd)
 until powershell Install-WindowsFeature Net-Framework-Core; do echo "trying again..."; done
-cinst -y python3
 cd ~/pybuild
 export exefile=Win64OpenSSL_Light-${BUILD_OPENSSL_VERSION//./_}.exe
 if [ ! -e $exefile ]; then
@@ -10,15 +9,16 @@ if [ ! -e $exefile ]; then
 fi
 echo "Installing $exefile..."
 powershell ".\\${exefile} /silent /sp- /suppressmsgboxes /DIR=C:\\ssl"
+cinst -y python3
+until cinst -y wixtoolset; do echo "trying again..."; done
 echo "OpenSSL dlls..."
 ls -alRF /c/ssl
 echo "c drive"
 ls -al /c
 echo "Python dlls..."
 ls -al /c/Python37/DLLs
-cp -v /c/ssl/*.dll /c/Python37/DLLs
+until cp -v /c/ssl/*.dll /c/Python37/DLLs; do echo "trying again..."; done
 export PATH=$PATH:/c/Python37/scripts
-until cinst -y wixtoolset; do echo "trying again..."; done
 cd $mypath
 pip install --upgrade pip
 pip freeze > upgrades.txt
