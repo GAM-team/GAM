a89e0936c23d8d3660aad7f8481ab97bebb990fc
Jay Lee <jay0lee@gmail.com>
2019-09-18 14:59:33 -0400

    OpenSSL 1.1.1d on Windows

--- src/travis/windows-x86_64-before-install.sh
+++ src/travis/windows-x86_64-before-install.sh
@@ -2,22 +2,22 @@ echo "Installing Net-Framework-Core..."
 export mypath=$(pwd)
 until powershell Install-WindowsFeature Net-Framework-Core; do echo "trying again..."; done
 cd ~/pybuild
-#export exefile=Win64OpenSSL_Light-${BUILD_OPENSSL_VERSION//./_}.exe
-#if [ ! -e $exefile ]; then
-#  echo "Downloading $exefile..."
-#  wget --quiet https://slproweb.com/download/$exefile
-#fi
-#echo "Installing $exefile..."
-#powershell ".\\${exefile} /silent /sp- /suppressmsgboxes /DIR=C:\\ssl"
+export exefile=Win64OpenSSL_Light-${BUILD_OPENSSL_VERSION//./_}.exe
+if [ ! -e $exefile ]; then
+  echo "Downloading $exefile..."
+  wget --quiet https://slproweb.com/download/$exefile
+fi
+echo "Installing $exefile..."
+powershell ".\\${exefile} /silent /sp- /suppressmsgboxes /DIR=C:\\ssl"
 cinst -y python3
 until cinst -y wixtoolset; do echo "trying again..."; done
-#echo "OpenSSL dlls..."
-#ls -alRF /c/ssl
-#echo "c drive"
-#ls -al /c
-#echo "Python dlls..."
-#ls -al /c/Python37/DLLs
-#until cp -v /c/ssl/*.dll /c/Python37/DLLs; do echo "trying again..."; done
+echo "OpenSSL dlls..."
+ls -alRF /c/ssl
+echo "c drive"
+ls -al /c
+echo "Python dlls..."
+ls -al /c/Python37/DLLs
+until cp -v /c/ssl/*.dll /c/Python37/DLLs; do echo "trying again..."; done
 export PATH=$PATH:/c/Python37/scripts
 cd $mypath
 pip install --upgrade pip
