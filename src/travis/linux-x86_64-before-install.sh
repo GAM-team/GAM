da3b16c0d45f92716c51f9762836e34463f56d80
Jay Lee <jay0lee@gmail.com>
2019-10-14 11:42:33 -0400

    watch cache

--- src/travis/linux-x86_64-before-install.sh
+++ src/travis/linux-x86_64-before-install.sh
@@ -27,6 +27,8 @@ else
     sudo apt-get -qq --yes update > /dev/null
     sudo apt-get -qq --yes build-dep python3 > /dev/null
     cd ~
+    echo "EVERYTHING IN HOME..."
+    ls -alRF
     cpucount=$(nproc --all)
     echo "This device has $cpucount CPUs for compiling..."
 
