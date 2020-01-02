7cbe37033dde550af108efaf20dd1739d072f425
Ross Scroggs <ross.scroggs@gmail.com>
2020-01-02 10:19:53 -0800

    Remove unneeded function utils.convertUTF8 (#1069)
    This was deprecated in move from Python 2 to 3

--- src/display.py
+++ src/display.py
@@ -1,18 +1,15 @@
 """Methods related to display of information to the user."""
 
 import sys
-import utils
 from var import ERROR_PREFIX
 from var import WARNING_PREFIX
 
 
 def print_error(message):
   """Prints a one-line error message to stderr in a standard format."""
-  sys.stderr.write(
-      utils.convertUTF8('\n{0}{1}\n'.format(ERROR_PREFIX, message)))
+  sys.stderr.write('\n{0}{1}\n'.format(ERROR_PREFIX, message))
 
 
 def print_warning(message):
   """Prints a one-line warning message to stderr in a standard format."""
-  sys.stderr.write(
-      utils.convertUTF8('\n{0}{1}\n'.format(WARNING_PREFIX, message)))
+  sys.stderr.write('\n{0}{1}\n'.format(WARNING_PREFIX, message))
