d144ce3135aae125bf5f233e1cfec029dcb7332c
Jay Lee <jay0lee@gmail.com>
2020-03-25 09:14:06 -0400

    set a minimum version for python/openssl

--- src/travis/osx-x86_64-before-install.sh
+++ src/travis/osx-x86_64-before-install.sh
@@ -3,7 +3,7 @@ whereibelong=$(pwd)
 #echo "Brew installing xz..."
 #brew install xz > /dev/null
 
-brew upgrade
+#brew upgrade
 
 # prefer standard GNU tools like date over MacOS defaults
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
