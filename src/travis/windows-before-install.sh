545c9ea8dd1d3b161af09486dfaef64caabc73db
Jay Lee <jay0lee@gmail.com>
2020-03-27 21:49:33 -0400

    fix 32-bit Win

--- src/travis/windows-before-install.sh
+++ src/travis/windows-before-install.sh
@@ -1,10 +1,12 @@
 if [[ "$PLATFORM" == "x86_64" ]]; then
   export BITS="64"
   export PYTHONFILE_BITS="-amd64"
+  export OPENSSL_BITS="-x64"
   export WIX_BITS="x64"
 elif [[ "$PLATFORM" == "x86" ]]; then
   export BITS="32"
   export PYTHONFILE_BITS=""
+  export OPENSSL_BITS=""
   export WIX_BITS="x86"
 fi
 echo "This is a ${BITS}-bit build for ${PLATFORM}"
@@ -41,8 +43,8 @@ if [ ! -e $exefile ]; then
   wget --quiet https://slproweb.com/download/$exefile
 fi
 until powershell ".\\${exefile} /silent /sp- /suppressmsgboxes /DIR=C:\\ssl"; do echo "trying openssl again..."; done
-until cp -v /c/ssl/libcrypto-1_1-x64.dll /c/python/DLLs/; do echo "trying libcrypto copy again..."; sleep 3; done
-until cp -v /c/ssl/libssl-1_1-x64.dll /c/python/DLLs/; do echo "trying libssl copy again..."; done
+until cp -v /c/ssl/libcrypto-1_1${OPENSSL_BITS}.dll /c/python/DLLs/; do echo "trying libcrypto copy again..."; sleep 3; done
+until cp -v /c/ssl/libssl-1_1${OPENSSL_BITS}.dll /c/python/DLLs/; do echo "trying libssl copy again..."; done
 if [[ "$PLATFORM" == "x86_64" ]]; then
   cp -v /c/python/DLLs/libssl-1_1-x64.dll /c/python/DLLs/libssl-1_1.dll
   cp -v /c/python/DLLs/libcrypto-1_1-x64.dll /c/python/DLLs/libcrypto-1_1.dll
