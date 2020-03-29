b677e8b4b217caf4018cb8c76c9391b55df173fc
Jay Lee <jay0lee@gmail.com>
2020-03-29 19:55:57 -0400

    Fix svars-write.py for Testing instances

--- src/travis/svars-write.py
+++ src/travis/svars-write.py
@@ -6,8 +6,8 @@ cfg = json.load(sys.stdin)
 cfg['client_secret'] = os.getenv('client_secret')
 jid = os.getenv('jid')
 cfg['refresh_token'] = os.getenv('refresh_%s' % jid)
-vmtype = os.getenv('VMTYPE')
-if vmtype == 'test':
+name = os.getenv('TRAVIS_JOB_NAME')
+if name.endswith('Testing'):
     out_file = 'oauth2.txt'
 else:
     out_file = 'gam/oauth2.txt'
