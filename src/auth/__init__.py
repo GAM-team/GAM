c83430a537531399dfdd1fb30e7357920c396a49
Jay Lee <jay0lee@gmail.com>
2020-04-06 14:18:37 -0400

    ensure get_admin_credentials only returns fresh creds

--- src/auth/__init__.py
+++ src/auth/__init__.py
@@ -1,10 +1,10 @@
 """Authentication/Credentials general purpose and convenience methods."""
 
-from . import oauth
+import transport
 from var import _FN_OAUTH2_TXT
 from var import GC_OAUTH2_TXT
 from var import GC_Values
-
+from . import oauth
 # TODO: Move logic that determines file name into this module. We should be able
 # to discover the file location without accessing a private member or waiting
 # for a global initialization.
@@ -12,16 +12,19 @@ DEFAULT_OAUTH_STORAGE_FILE = _FN_OAUTH2_TXT
 
 
 def get_admin_credentials_filename():
-  """Gets the name of the file that stores the admin account credentials."""
-  # If the environment globals are loaded, use the set global value. It may have
-  # some custom name in it. Otherwise, just use the default name.
-  if GC_Values[GC_OAUTH2_TXT]:
-    return GC_Values[GC_OAUTH2_TXT]
-  else:
+    """Gets the name of the file that stores the admin account credentials."""
+    # If the environment globals are loaded, use the set global value. It may have
+    # some custom name in it. Otherwise, just use the default name.
+    if GC_Values[GC_OAUTH2_TXT]:
+        return GC_Values[GC_OAUTH2_TXT]
     return DEFAULT_OAUTH_STORAGE_FILE
 
 
 def get_admin_credentials():
-  """Gets oauth.Credentials that are authenticated as the domain's admin user."""
-  credential_file = get_admin_credentials_filename()
-  return oauth.Credentials.from_credentials_file(credential_file)
+    """Gets oauth.Credentials that are authenticated as the domain's admin user."""
+    credential_file = get_admin_credentials_filename()
+    creds = oauth.Credentials.from_credentials_file(credential_file)
+    if creds.expired:
+        request = transport.create_request()
+        creds.refresh(request)
+    return creds
