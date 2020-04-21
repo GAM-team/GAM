ea223007ceff7efc5f34f62ca34bc9b563d00850
Erik Jochman <34144949+ejochman@users.noreply.github.com>
2020-04-21 10:50:07 -0700

    Add gam.py to /src for backwards compatibility
    A stub that calls gam.__main__.main() to be used by users who are not
    with the syntax of calling a module implementation. It should also
    provide immediate backwards-compatibility with existing scripts with
    references to this file.

---
+++ src/gam.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Provides backwards compatibility for calling gam as a single .py file"""
+
+import sys
+
+from gam.__main__ import main
+
+# Run from command line
+if __name__ == "__main__":
+    main(sys.argv)
