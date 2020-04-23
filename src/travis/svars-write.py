7dfdf4cdbdfa27047fbbef552af63738753153df
Jay Lee <jay0lee@gmail.com>
2020-04-23 15:39:56 -0400

    fix svars-write.py

--- src/travis/svars-write.py
+++ src/travis/svars-write.py
@@ -6,10 +6,7 @@ cfg = json.load(sys.stdin)
 cfg['client_secret'] = os.getenv('client_secret')
 jid = os.getenv('jid')
 cfg['refresh_token'] = os.getenv('refresh_%s' % jid)
-name = os.getenv('TRAVIS_JOB_NAME')
-if name.endswith('Testing'):
-    out_file = 'oauth2.txt'
-else:
-    out_file = 'gam/oauth2.txt'
+gampath = os.getenv('gampath')
+out_file = os.join(gampath, 'oauth2.txt')
 with open(out_file, 'w') as f:
   json.dump(cfg, f)
