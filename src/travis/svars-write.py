7df2293f1b87cedf0673da8c036155d87d5a14d5
Jay Lee <jay0lee@gmail.com>
2020-04-23 15:56:06 -0400

    os.path.join...

--- src/travis/svars-write.py
+++ src/travis/svars-write.py
@@ -7,6 +7,6 @@ cfg['client_secret'] = os.getenv('client_secret')
 jid = os.getenv('jid')
 cfg['refresh_token'] = os.getenv('refresh_%s' % jid)
 gampath = os.getenv('gampath')
-out_file = os.join(gampath, 'oauth2.txt')
+out_file = os.path.join(gampath, 'oauth2.txt')
 with open(out_file, 'w') as f:
   json.dump(cfg, f)
