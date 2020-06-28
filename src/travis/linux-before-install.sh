22fc54b2fab0cb0072d9eae6e97cb64e0fbb5fd4
Jay Lee <jay0lee@gmail.com>
2020-06-28 19:02:24 -0400

    fix patchelf download/extract

--- src/travis/linux-before-install.sh
+++ src/travis/linux-before-install.sh
@@ -97,8 +97,8 @@ else
     echo "Installing deps for StaticX..."
     if [ ! -d patchelf-$PATCHELF_VERSION ]; then
       echo "Downloading PatchELF $PATCHELF_VERSION"
-      wget https://nixos.org/releases/patchelf/patchelf-$PATCHELF_VERSION/patchelf-$PATCHELF_VERSION.tar.bz2
-      tar xf patchelf-$PATCHELF_VERSION.tar.bz2
+      wget https://github.com/NixOS/patchelf/archive/$PATCHELF_VERSION.tar.gz 
+      tar xf $PATCHELF_VERSION.tar.gz
       cd patchelf-$PATCHELF_VERSION
       ./configure
       make
