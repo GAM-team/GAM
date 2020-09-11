f57ec3858be161d1e716b8f2566749ac1e996d95
Ross Scroggs <ross.scroggs@gmail.com>
2020-09-11 08:03:42 -0700

    Update __init__.py
    Is this what yuou meant?

--- src/gam/auth/__init__.py
+++ src/gam/auth/__init__.py
@@ -22,10 +22,10 @@ def get_admin_credentials_filename():
     """Gets the name of the file that stores the admin account credentials."""
     # If the environment globals are loaded, use the set global value. It may have
     # some custom name in it. Otherwise, just use the default name.
-    if not GC_Values[GC_ENABLE_DASA]:
-        return GC_Values[GC_OAUTH2_TXT] if GC_Values[GC_OAUTH2_TXT] else _FN_OAUTH2_TXT
-    else:
+    if GC_Values[GC_ENABLE_DASA]:
         return GC_Values[GC_OAUTH2SERVICE_JSON] if GC_Values[GC_OAUTH2SERVICE_JSON] else _FN_OAUTH2SERVICE_JSON
+    else:
+        return GC_Values[GC_OAUTH2_TXT] if GC_Values[GC_OAUTH2_TXT] else _FN_OAUTH2_TXT
 
 
 def get_admin_credentials(api=None):
@@ -35,11 +35,12 @@ def get_admin_credentials(api=None):
         raise oauth.InvalidCredentialsFileError
     with open(credential_file, 'r') as f:
         creds_data = json.load(f)
-    if not GC_Values[GC_ENABLE_DASA] and 'token' in creds_data:
-        return oauth.Credentials.from_credentials_file(credential_file)
-    elif GC_Values[GC_ENABLE_DASA] and 'private_key' in creds_data:
+    # Validate that enable DASA matches content of authorization file
+    if GC_Values[GC_ENABLE_DASA] and 'private_key' in creds_data:
         audience = f'https://{api}.googleapis.com/'
         return JWTCredentials.from_service_account_info(creds_data,
                                                         audience=audience)
+    elif not GC_Values[GC_ENABLE_DASA] and 'token' in creds_data:
+        return oauth.Credentials.from_credentials_file(credential_file)
     else:
         raise oauth.InvalidCredentialsFileError
