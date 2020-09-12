f340355c1e905b1fa39d02869b9ce0859157d799
Ross Scroggs <ross.scroggs@gmail.com>
2020-09-12 08:45:10 -0700

    Complete create admintole

--- src/gam/gapi/directory/roles.py
+++ src/gam/gapi/directory/roles.py
@@ -1,6 +1,7 @@
 import sys
 
 from gam.var import GC_Values, GC_CUSTOMER_ID
+from gam import controlflow
 from gam import display
 from gam import gapi
 from gam.gapi import directory as gapi_directory
@@ -9,31 +10,40 @@ from gam.gapi.directory import privileges as gapi_directory_privileges
 
 def create():
     cd = gapi_directory.build()
-    body = {'privileges': []}
+    body = {'roleName': sys.argv[3], 'rolePrivileges': []}
     all_privileges = gapi_directory_privileges.print_(return_only=True)
-    i = 3
+    i = 4
     while i < len(sys.argv):
         myarg = sys.argv[i].lower()
         if myarg == 'privileges':
-            privs = sys.argv[i + 1]
-            if privs == 'all':
-                body['rolePrivileges'] = all_privileges
-            elif privs == 'all_ou':
+            privs = sys.argv[i + 1].upper()
+            if privs == 'ALL':
                 body['rolePrivileges'] = [
-                    p for p in all_privileges if p.get('isOuScopable')
-                ]
+                    {'privilegeName': p['privilegeName'], 'serviceId': p['serviceId']} for p in all_privileges
+                    ]
+            elif privs == 'ALL_OU':
+                body['rolePrivileges'] = [
+                    {'privilegeName': p['privilegeName'], 'serviceId': p['serviceId']} for p in all_privileges if p.get('isOuScopable')
+                    ]
             else:
-                # Known broken, need to get serviceName in here also...
-                body['rolePrivileges'] = [{
-                    'privilegeName': p
-                } for p in sys.argv[i + 1].split(',')]
+              for priv in privs.split(','):
+                  for p in all_privileges:
+                      if priv == p['privilegeName']:
+                        body['rolePrivileges'].append({'privilegeName': p['privilegeName'], 'serviceId': p['serviceId']})
+                        break
+                  else:
+                      controlflow.invalid_argument_exit(priv,
+                                                        'gam create adminrole privileges')
             i += 2
-        elif myarg == 'name':
-            body['roleName'] = sys.argv[i + 1]
+        elif myarg == 'description':
+            body['roleDescription'] = sys.argv[i + 1]
             i += 2
         else:
             controlflow.invalid_argument_exit(sys.argv[i],
                                               'gam create adminrole')
+    if not body['rolePrivileges']:
+        controlflow.missing_argument_exit('privileges',
+                                          'gam create adminrole')
     print(f'Creating role {body["roleName"]}')
     gapi.call(cd.roles(),
               'insert',
