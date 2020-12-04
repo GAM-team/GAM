181a2f8949784a7c2bef38332c5a0e81bc395d1c
Jay Lee <jay0lee@gmail.com>
2020-12-04 18:41:47 -0500

    actions take 100

--- .github/actions/decrypt.sh
+++ .github/actions/decrypt.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
 mydir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
+echo "running from ${mydir}"
 credsfile="${gampath}/creds.tar"
-gpg --quiet --batch --yes --decrypt --passphrase="${PASSPHRASE}" \
-    --output "${credsfile}" "${mydir}/creds.tar.gpg"
+echo "target file is ${credsfile}"
+gpgfile="${mypath}/creds.tar.gpg"
+echo "source file is ${credsfile}"
+if [ -z ${PASSCODE+x} ]; then
+  echo "PASSCODE is unset";
+else
+  echo "PASSCODE is set'";
+fi
+
+gpg --quiet --batch --yes --decrypt --passphrase="${PASSCODE}" \
+    --output "${credsfile}" "${gpgfile}"
 
 tar xf "${credsfile}" --directory "${gampath}"
