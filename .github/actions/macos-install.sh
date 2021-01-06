a8203baa50eb873157851da794f5e744e30d2466
Jay Lee <jay0lee@gmail.com>
2021-01-06 09:20:10 -0500

    another attempt at arm64 MacOS support

--- .github/actions/macos-install.sh
+++ .github/actions/macos-install.sh
@@ -4,14 +4,19 @@ echo "Xcode versionn:"
 xcodebuild -version
 export gampath=dist/gam
 rm -rf $gampath
-$python -OO -m PyInstaller --clean --noupx --strip -F --distpath $gampath gam.spec
-export gam="$gampath/gam"
+if [ "$PLATFORM" == "x86_64" ]; then
+    export specfile="gam.spec"
+else
+    export specfile="gam-universal2.spec"
+fi
+$python -OO -m PyInstaller --clean --noupx --strip -F --distpath "${gampath}" "${specfile}"
+export gam="${gampath}/gam"
 $gam version extended
 export GAMVERSION=`$gam version simple`
-cp LICENSE $gampath
-cp GamCommands.txt $gampath
+cp LICENSE "${gampath}"
+cp GamCommands.txt "${gampath}"
 MACOSVERSION=$(defaults read loginwindow SystemVersionStampAsString)
-GAM_ARCHIVE=gam-$GAMVERSION-$GAMOS-$PLATFORM-MacOS$MACOSVERSION.tar.xz
-rm $gampath/lastupdatecheck.txt
+GAM_ARCHIVE="gam-${GAMVERSION}-${GAMOS}-${PLATFORM}-MacOS${MACOSVERSION}.tar.xz"
+rm "${gampath}/lastupdatecheck.txt"
 # tar will cd to dist/ and tar up gam/
 tar -C dist/ --create --file $GAM_ARCHIVE --xz gam
