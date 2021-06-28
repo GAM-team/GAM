400b4af769512e8c124b180d5d8fd4414256e4ca
Jay Lee <jay0lee@gmail.com>
2021-06-28 15:35:40 -0400

    Update macos-before-install.sh

--- .github/actions/macos-before-install.sh
+++ .github/actions/macos-before-install.sh
@@ -22,11 +22,7 @@ cd ~
 
 # Use official Python.org version of Python which is backwards compatible
 # with older MacOS versions
-if [ "$PLATFORM" == "x86_64" ]; then
-  export pyfile=python-$BUILD_PYTHON_VERSION-macosx10.9.pkg
-else
-  export pyfile=python-$BUILD_PYTHON_VERSION-macos11.pkg
-fi
+export pyfile=python-$BUILD_PYTHON_VERSION-macos11.pkg
 
 wget https://www.python.org/ftp/python/$BUILD_PYTHON_VERSION/$pyfile
 echo "installing Python $BUILD_PYTHON_VERSION..."
