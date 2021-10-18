eeab09eacb973ad904a5d581d5a9449775f7f223
Jay Lee <jay0lee@gmail.com>
2021-10-18 08:50:13 -0400

    fix deprecated package in a_atleast_b.py

--- src/tools/a_atleast_b.py
+++ src/tools/a_atleast_b.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 
-#from packaging import version
-from distutils.version import LooseVersion
+from packaging import version
 import sys
 
 a = sys.argv[1]
 b = sys.argv[2]
-#result = version.parse(a) >= version.parse(b)
-result = LooseVersion(a) >= LooseVersion(b)
+result = version.parse(a) >= version.parse(b)
 if result:
     print('OK: %s is equal or newer than %s' % (a, b))
 else:
