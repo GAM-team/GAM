881641e2b4e68c51e174118ec66c55192bd040c7
Jay Lee <jay0lee@gmail.com>
2022-04-07 19:53:11 +0000

    gapi_drive

---
+++ src/gam/gapi/drive/__init__.py
@@ -0,0 +1,8 @@
+import gam
+
+
+def build(user=None):
+    if not user:
+        user = gam._get_admin_email()
+    userEmail = gam.convertUIDtoEmailAddress(user)
+    return (userEmail, gam.buildGAPIServiceObject('drive3', userEmail))
