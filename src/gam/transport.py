5e1df9263bdfde5cfc6c86ab40c794dd4c86d228
Jay Lee <jay0lee@gmail.com>
2023-04-05 17:18:10 +0000

    use transport and bump retries

--- src/gam/transport.py
+++ src/gam/transport.py
@@ -9,6 +9,8 @@ from gam.var import GC_TLS_MAX_VERSION
 from gam.var import GC_TLS_MIN_VERSION
 from gam.var import GC_Values
 
+# Bump default retries
+httplib2.RETRIES = 5
 
 def create_http(cache=None,
                 timeout=None,
