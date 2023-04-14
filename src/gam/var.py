c5734beef679a7ba49315fca800e8785a632e0c8
Jay Lee <jay0lee@gmail.com>
2023-04-14 18:53:52 -0400

    Update var.py

--- src/gam/var.py
+++ src/gam/var.py
@@ -8,7 +8,7 @@ import platform
 import re
 
 GAM_AUTHOR = 'Jay Lee <jay0lee@gmail.com>'
-GAM_VERSION = '6.56'
+GAM_VERSION = '6.57'
 GAM_LICENSE = 'Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)'
 
 GAM_URL = 'https://jaylee.us/gam'
