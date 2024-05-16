c9996f4942c2ab8fdb66d5fe89d5b8e4de56e939
Ross Scroggs <ross.scroggs@gmail.com>
2024-05-16 09:59:16 -0700

    MacOS 14.5 curl doesn't like empty arguments

--- src/gam-install.sh
+++ src/gam-install.sh
@@ -161,13 +161,14 @@ else
 fi
 
 if [ -z ${GHCLIENT+x} ]; then
-  check_type="unauthenticated"
-else
+  check_type="unauthenticated"e
+  echo_yellow "Checking GitHub URL $release_url for $gamversion GAM release ($check_type)..."
+  release_json=$(curl -s "$release_url" 2>&1 /dev/null)
+lse
   check_type="authenticated"
+  echo_yellow "Checking GitHub URL $release_url for $gamversion GAM release ($check_type)..."
+  release_json=$(curl -s "$GHCLIENT" "$release_url" 2>&1 /dev/null)
 fi
-
-echo_yellow "Checking GitHub URL $release_url for $gamversion GAM release ($check_type)..."
-release_json=$(curl -s "$GHCLIENT" "$release_url" 2>&1 /dev/null)
 
 echo_yellow "Getting file and download URL..."
 # Python is sadly the nearest to universal way to safely handle JSON with Bash
