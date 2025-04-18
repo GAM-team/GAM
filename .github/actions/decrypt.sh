7f72dad9b84fd33d07983b484602a5c3f9272dba
Jay Lee <jay0lee@gmail.com>
2025-04-18 11:45:39 -0400

    Update decrypt.sh

--- .github/actions/decrypt.sh
+++ .github/actions/decrypt.sh
@@ -4,5 +4,16 @@ if [ ! -d "$credspath" ]; then
     echo "creating ${credspath}"
     mkdir -p "$credspath"
 fi
-
-echo -e "$oa2" > "${credspath}/oauth2.txt"
+credsfile="${credspath}/oauth2.txt"
+echo "$oa2" > "$credsfile"
+echo "File size:"
+wc -c "$credsfile"
+echo "File type:"
+file "$credsfile"
+echo "Validation:"
+jq -e . "$credsfile" > /dev/null
+if [ $? -eq 0 ]; then
+  echo "Valid JSON"
+else
+  echo "Invalid JSON"
+fi
